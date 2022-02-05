d81173317a40726fbcf953dc1ba6d80a744c4bbf
qizzle <52245845+qizzle@users.noreply.github.com>
2022-02-05 21:33:18 +0000

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,127 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    autoHideMenuBar: true
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
+ipcMain.on("openDialog", (event, arg) => {
+  dialog.showOpenDialog({ properties: ["openDirectory"] }).then((dir) => {
+    event.reply("folderPath", dir)
+  })
+});
+
+//Uses node.js process manager
+const child_process = require('child_process');
+const { fstat } = require('fs');
+
+// This function will output the lines from the script 
+// and will return the full combined output
+// as well as exit code when it's done (using the callback).
+function run_script(command, args, callback) {
+    var child = child_process.spawn(command, args, {
+        encoding: 'utf8',
+        shell: true
+    });
+    // You can also use a variable to save the output for when the script closes later
+    child.on('error', (error) => {
+        dialog.showMessageBox({
+            title: 'Title',
+            type: 'warning',
+            message: 'Error occured.\r\n' + error
+        });
+    });
+
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', (data) => {
+        //Here is the output
+        data=data.toString();   
+        console.log(data);      
+    });
+
+    child.stderr.setEncoding('utf8');
+    child.stderr.on('data', (data) => {
+        // Return some data to the renderer process with the mainprocess-response ID
+        mainWindow.webContents.send('mainprocess-response', data);
+        //Here is the output from the command
+        console.log(data);  
+    });
+
+    child.on('close', (code) => {
+        //Here you can get the exit code of the script
+        if (typeof callback === 'function') {
+          callback();
+        }
+    });
+    
+};
+
+ipcMain.on("install", async (event, arg) => {
+  run_script("npx", [`asar extract "${arg + "/app.asar"}" "${arg + "/app"}"`], async function() {
+    await fs.mkdirSync(path.join(arg, "/bdbs")); // Creating bdbs folder
+    fs.mkdirSync(path.join(arg, "/bdbs/plugins"));
+    fs.mkdirSync(path.join(arg, "/bdbs/themes"));
+    fs.mkdirSync(path.join(arg, "/bdbs/settings"));
+    let json = {
+      theme: "light"
+    };
+    fs.writeFileSync(path.join(arg, "/bdbs/settings/settings.json"), JSON.stringify(json));
+    //fs.unlinkSync(path.join(arg, "/app.asar")); -- Deletes app.asar
+    fs.copyFileSync(path.join(__dirname, "/bdbs/bdbs.html"), path.join(arg, "/app/bdbs.html"));
+    fs.copyFileSync(path.join(__dirname, "/bdbs/preload.js"), path.join(arg, "/app/preload.js"));
+
+    fs.copyFileSync(path.join(__dirname, "/bdbs/light.css"), path.join(arg, "/bdbs/themes/light.css"));
+    fs.copyFileSync(path.join(__dirname, "/bdbs/light.theme.json"), path.join(arg, "/bdbs/themes/light.theme.json"));
+    fs.copyFileSync(path.join(__dirname, "/bdbs/dark.css"), path.join(arg, "/bdbs/themes/dark.css"));
+    fs.copyFileSync(path.join(__dirname, "/bdbs/dark.theme.json"), path.join(arg, "/bdbs/themes/dark.theme.json"));
+
+    event.reply("installed"); // -> index.html
+  });
+});
